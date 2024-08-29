ad327c827d933be23d58480b4b5dd4316ca9470d
Suzana Pachelle <suzi.pachelle@gmail.com>
2024-08-29 15:22:54 -0300

    Change deploy file

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-
 echo "Running composer"
-composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
-
-echo "Clearing caches..."
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
 php artisan config:cache
@@ -16,7 +11,3 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
-
-echo "Setting file permissions..."
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
