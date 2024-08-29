ba6ff1cc316acd8937290c18dba66cf50693d9ea
Suzana Pachelle <suzi.pachelle@gmail.com>
2024-08-29 15:04:54 -0300

    Corrigindo arquivos

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+
 echo "Running composer"
-composer global require hirak/prestissimo
-composer install --no-dev
+composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+
+echo "Clearing caches..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 
 echo "Caching config..."
 php artisan config:cache
@@ -11,3 +16,7 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Setting file permissions..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
